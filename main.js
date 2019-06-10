2915369ed94ada0aec5a203b01e498b6fc217b66
Arhen Santiago <lincoldarhen9900@gmail.com>
2019-06-10 14:58:12 +0800

    sliding form

---
+++ main.js
@@ -0,0 +1,10 @@
+const container = document.getElementById("container");
+const signIn = document.getElementById("signIn");
+const signUp = document.getElementById("signUp");
+
+signUp.addEventListener("click", () =>
+	container.classList.add("right-panel-active"),
+);
+signIn.addEventListener("click", () =>
+	container.classList.remove("right-panel-active"),
+);
